95bdc671542ebe0c1284437b79d2a87f8bed47d7
Spandan <sbmailid1@gmail.com>
2022-05-21 08:12:09 +0530

    modify

---
+++ app/src/main/java/com/basu/foodtofeed/HungerHotspotActivity.java
@@ -0,0 +1,14 @@
+package com.basu.foodtofeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HungerHotspotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hunger_hotspot);
+    }
+}
--- app/src/main/java/com/basu/foodtofeed/MainActivity.java
+++ app/src/main/java/com/basu/foodtofeed/MainActivity.java
@@ -2,7 +2,10 @@ package com.basu.foodtofeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RelativeLayout donateFood= findViewById(R.id.donate);
+        RelativeLayout locateHotspot= findViewById(R.id.hotspot);
+        RelativeLayout healthyNutrition= findViewById(R.id.healthy);
+
+        donateFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DonateFoodActivity.class));
+            }
+        });
+        locateHotspot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HungerHotspotActivity.class));
+            }
+        });
+        healthyNutrition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, HealthyNutritionActivity.class));
+            }
+        });
     }
 }
